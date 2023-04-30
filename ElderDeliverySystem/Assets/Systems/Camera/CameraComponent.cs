75ab881e5c3ab8a1a786e8018d307aa8bcff8168
Emil Bohleber <me@limered.de>
2023-04-30 02:19:13 +0200

    add camera movement

---
+++ ElderDeliverySystem/Assets/Systems/Camera/CameraComponent.cs
@@ -0,0 +1,12 @@
+﻿using SystemBase.Core.Components;
+using Unity.Mathematics;
+
+namespace Systems.Camera
+{
+    [SingletonComponent]
+    public class CameraComponent : GameComponent
+    {
+        public float2 extents;
+        public float height;
+    }
+}
---
+++ ElderDeliverySystem/Assets/Systems/Camera/CameraSystem.cs
@@ -0,0 +1,40 @@
+﻿using SystemBase.Core.GameSystems;
+using Systems.Player;
+using UniRx;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace Systems.Camera
+{
+    [GameSystem]
+    public class CameraSystem : GameSystem<CameraComponent>
+    {
+        public override void Register(CameraComponent component)
+        {
+            SharedComponentCollection
+                .Subscribe<PlayerComponent>(p => StartFollowing(p, component))
+                .AddTo(component);
+        }
+
+        private void StartFollowing(PlayerComponent player, CameraComponent camera)
+        {
+            SystemUpdate()
+                .Subscribe(_ => UpdateCameraPosition(camera, player))
+                .AddTo(camera);
+        }
+
+        private void UpdateCameraPosition(CameraComponent camera, PlayerComponent player)
+        {
+            var playerPos = player.transform.position;
+            var cameraPos = camera.transform.position;
+            var delta = playerPos - cameraPos;
+            var delta2D = new Vector2(delta.x, delta.z);
+            if(math.abs(delta2D.x) > camera.extents.x)
+                cameraPos += Vector3.right * (delta2D.x - math.sign(delta2D.x) * camera.extents.x);
+            if(math.abs(delta2D.y) > camera.extents.y)
+                cameraPos += Vector3.forward * (delta2D.y - math.sign(delta2D.y) * camera.extents.y);
+            
+            camera.transform.position = cameraPos;
+        }
+    }
+}
