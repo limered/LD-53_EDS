4c4a6dad58a5d18d2d9cfdbbbb30e6d6bb316c99
Emil Bohleber <me@limered.de>
2023-04-30 15:01:01 +0200

    add some victims

---
+++ ElderDeliverySystem/Assets/Systems/Victims/VictimComponent.cs
@@ -0,0 +1,14 @@
+﻿using SystemBase.Core.Components;
+using Systems.Movement;
+using UnityEngine;
+
+namespace Systems.Victims
+{
+    [RequireComponent(typeof(BodyComponent))]
+    public class VictimComponent  : GameComponent
+    {
+        public float minDistanceToPlayer;
+        
+        public BodyComponent BodyComponent { get;set; }
+    }
+}
---
+++ ElderDeliverySystem/Assets/Systems/Victims/VictimSystem.cs
@@ -0,0 +1,35 @@
+﻿using SystemBase.Core.GameSystems;
+using SystemBase.Utils;
+using Systems.Movement;
+using Systems.Player;
+using UniRx;
+using UnityEngine;
+
+namespace Systems.Victims
+{
+    [GameSystem(typeof(PlayerControlSystem))]
+    public class VictimSystem : GameSystem<VictimComponent>
+    {
+        public override void Register(VictimComponent component)
+        {
+            component.BodyComponent = component.GetComponent<BodyComponent>();
+            
+            SystemUpdate(component)
+                .Subscribe(_ => CheckForPlayer(component))
+                .AddTo(component);
+        }
+
+        private void CheckForPlayer(VictimComponent component)
+        {
+            if (SharedComponentCollection.TryGet<PlayerComponent>(out var player))
+            {
+                var distance = player.transform.position - component.transform.position;
+                var distance2D = distance.XZ();
+                if (distance2D.magnitude < component.minDistanceToPlayer)
+                {
+                    component.BodyComponent.AddForce(-distance.normalized * player.speed);
+                }
+            }
+        }
+    }
+}
