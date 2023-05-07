3f8de5d6baa3bdd19e961277cd7e028e86d51cbf
Emil Bohleber <me@limered.de>
2023-05-07 19:34:08 +0200

    rework spawning

--- ElderDeliverySystem/Assets/Systems/Victims/VictimSystem.cs
+++ ElderDeliverySystem/Assets/Systems/Victims/VictimSystem.cs
@@ -1,4 +1,5 @@
-﻿using SystemBase.Core.GameSystems;
+﻿using System.Collections;
+using SystemBase.Core.GameSystems;
 using SystemBase.GameState.States;
 using SystemBase.Utils;
 using Systems.Movement;
@@ -80,15 +81,34 @@ namespace Systems.Victims
                 .AddTo(component);
         }
 
-        private void SpawnVictim(WorldComponent component)
+        private void SpawnVictim(WorldComponent world)
         {
             if (Time.deltaTime >= FifteenFps) return;
+            if (world.LastSpawnTime + world.spawnInterval > Time.time) return;
+            world.LastSpawnTime = Time.time;
 
-            if (component.LastSpawnTime + component.spawnInterval > Time.time) return;
-            component.LastSpawnTime = Time.time;
-            var randomSpawnPosition = new Vector3(Random.Range(-component.extents.x, component.extents.x), 0,
-                Random.Range(-component.extents.y, component.extents.y));
-            Object.Instantiate(component.victimPrefab, randomSpawnPosition, Quaternion.identity, component.transform);
+            var randomSpawnCenter = new Vector2(
+                Random.Range(-world.extents.x, world.extents.x),
+                Random.Range(-world.extents.y, world.extents.y));
+
+            Observable.FromCoroutine(_ => SpawnVictims(world.victimPrefab, world, randomSpawnCenter))
+                .Subscribe();
+        }
+
+        private IEnumerator SpawnVictims(
+            GameObject prefab,
+            WorldComponent world,
+            Vector2 randomSpawnCenter)
+        {
+            for (var i = 0; i < world.spawnCount; i++)
+            {
+                var spawnPosition = randomSpawnCenter + Random.insideUnitCircle * world.spawnAreaRadius;
+
+                Object.Instantiate(prefab, new Vector3(spawnPosition.x, 0, spawnPosition.y), Quaternion.identity,
+                    world.transform);
+
+                yield return null;
+            }
         }
     }
 }
